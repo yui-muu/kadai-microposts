58b10255445809eea993bd8f7541182728b0aa28
yui.murayama <ravu34zu@yahoo.co.jp>
2020-06-18 06:05:37 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -24,5 +22,7 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
 });
+
